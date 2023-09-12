3cb79c4dd9a4f7d249d5508e2cc995e78b94f0ee
Geraniel <geraniel.motta@monitoratec.com.br>
2023-09-12 15:40:02 -0300

    criação do componente de cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'listarPensamento'
+  },
+  {
+    path:'criarPensamento',
+    component: CriarPensamentoComponent
+  },
+  {
+    path:'listarPensamento',
+    component: CriarPensamentoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
